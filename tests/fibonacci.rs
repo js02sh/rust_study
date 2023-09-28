66d757290f286a2ea6f98c87daf87da304815359
Jisang Yun <journeyofbabo@gmail.com>
2023-09-28 23:40:55 +0900

    easy version

--- tests/fibonacci.rs
+++ tests/fibonacci.rs
@@ -10,34 +10,26 @@ fn fibonacci(n: u64) -> usize {
 }
 
 fn main() {
+    println!("------------------------------------");
     println!("Welcome to the Fibonacci calculator");
     loop {
+        println!("------------------------------------");
         println!("Put number for nth Fibonacci number:");
         let mut n = String::new();
 
-        match io::stdin().read_line(&mut n) {
-            Ok(_) => {
-                let n = n.trim(); // Remove newline character
-                if n == "q" {
-                    println!("Break");
-                    break;
-                } else {
-                    // Parse the input as a u64
-                    let n: u64 = n.parse().expect("Please write valid input");
+        io::stdin()
+            .read_line(&mut n)
+            .expect("Faild to read line");
+        let n:u64 = n.trim().parse()
+            .expect("Please input valid number");
 
-                    let fib = fibonacci(n);
-
-                    for i in 1..=n {
-                        print!("{} ", fibonacci(i));
-                    }
-                    println!("\nThe nth Fibonacci number is {}", fib);
-                    println!("Press 'q' to finish\n");
-                }
-            }
-            Err(_err) => {
-                println!("Failed to read line");
+        for i in 1..=n {
+            print!("{} ", fibonacci(i));
+            if i == n {
+                println!("\nThe nth Fibonacci number is {}\n", fibonacci(i));
             }
         }
+
     }
 }
 
