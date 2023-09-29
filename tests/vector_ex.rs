ece6d1193d32dc33a1f93656b180f5df99fcd8d5
Jisang Yun <journeyofbabo@gmail.com>
2023-09-30 00:49:48 +0900

    learn vector

---
+++ tests/vector_ex.rs
@@ -0,0 +1,43 @@
+
+
+
+fn main() {
+    //let v: Vec<i32> = Vec::new();
+    //let mut v = Vec::new();
+    let v = vec![1,2,3,4,5,6,7];
+
+    // v.push(5);
+    // v.push(7);
+
+    println!("{:?}", v);
+
+    match v.get(2) {
+        Some(t) => println!("The third element is {}", t),
+        None => println!("There is no third element"),
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    for (i, element) in v.iter().enumerate() {
+        println!("index: {}, element: {}", i, element);
+    }
+
+    println!("{:?}", v);
+
+
+}
+
+
+
+//----------------------
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_miru() {
+        main();
+    }
+}
