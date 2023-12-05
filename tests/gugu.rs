bf232295a10c67007ca69faa353f1decfb587600
Jisang Yun <journeyofbabo@gmail.com>
2023-12-05 15:11:57 +0900

    remove , with mecro

--- tests/gugu.rs
+++ tests/gugu.rs
@@ -6,7 +6,14 @@ fn main() {
         for j in 1..10 {
             print!("{:3},", i * j); // {:3} 하게되면 오른쪽 정렬, 3칸 부여?
         }
-        println!("");
+        println!("\n");
+    }
+
+    for y in 1..10 {
+        let s = (1..10)
+            .map(|x| format!("{:3}", x * y))
+            .collect::<Vec<String>>().join(",");
+        println!("{}", s);
     }
 }
 
